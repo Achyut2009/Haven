7c8ee0518cfd2b54a002a836f88201700da1e61d
Achyut Paliwal <achyutkpaliwal@gmail.com>
2026-01-12 15:15:39 +0300

    10th commit

---
+++ app/data.tsx
@@ -0,0 +1,23 @@
+type judges = {
+    name: string,
+    title: string[],
+    image: string,
+}
+
+export const judges =[
+    {
+        name: "Achyut Paliwal",
+        title: ["conditional rendering", "hello world"],
+        image: "/judge1.png",
+    },
+    {
+        name: "Anik Dash",
+        title: ["Profficient Programmer"],
+        image: "/judge2.png",
+    },
+    {
+        name: "Aditya Shukla",
+        title: ["Student at NIT-Warangal"],
+        image: "/judge3.png",
+    },
+];
